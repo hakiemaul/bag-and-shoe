9ffa57254e11858806bdac63d709a347df91b9c9
hakiemaul <noersedya@gmail.com>
2017-07-03 08:27:20 +0700

    backend done

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
 const api = require('./routes')
 
+mongoose.connect('mongodb://localhost/bag-and-shoe')
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/', api)
 
 app.listen(3000)
